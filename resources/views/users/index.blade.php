15be15121fe810604d73b17f7373b4ba841045fa
ceejltayco <ceejltayco@gmail.com>
2023-01-21 16:13:20 +0800

    laravel session CRUD

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
+        <div class="container" style="width: 70%; margin-bottom: 5px; padding: 0; text-align: right;">
+            <a href=" {{route('users.create')}} " class="btn btn-success">Create User</a>
+        </div>
         <div class="card m-auto" style="width: 70%">
             <div class="card-body">
                 {{-- Table --}}
-
                 <table class="table">
                     <thead>
                         <tr>
@@ -12,6 +14,7 @@
                             <th>Name</th>
                             <th>Email</th>
                             <th>Created At</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -21,6 +24,15 @@
                                 <td> {{$user->name}} </td>
                                 <td> {{ $user->email }} </td>
                                 <td> {{ $user->created_at }} </td>
+                                <td>
+                                    <a href=" {{route('users.edit', ['id' => $user->id])}} " class="btn btn-warning mr-1" style="margin-right: 5px">Edit</a>
+                                    <form 
+                                    method="POST" action="{{ route('users.destroy', ['id' => $user->id]) }}" 
+                                    style="display: inline;">
+                                        @csrf
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,15 @@ Route::get('/test-page', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\UserController::class, 'index'])->name('home');
+Route::prefix('users')->group(function() {
+    Route::get('create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
+    Route::get('edit/{id}', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
+    Route::post('', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
+    Route::post('{id}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
+    Route::post('destroy/{id}', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
+});
+
+// is the same with
+// Route::get('users/create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
+// Route::get('users/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
 
